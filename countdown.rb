32c4c41200d1dc362a0c1af7163dc015bd526565
Wilson Kinyua <106533034+wilsonkinyuam@users.noreply.github.com>
2022-08-22 22:38:14 +0300

    Update countdown.rb

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    x = 1
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        x -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+    countdown(number)
+    sleep(3)
+end
+
+countdown_with_sleep(10)
